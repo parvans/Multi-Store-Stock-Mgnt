48519c6ecc3c4c0a6031e21df8385d9276d5ac1f
parvans <parvansajeevan@gmail.com>
2026-07-11 17:26:54 +0530

    - role based middleware added

---
+++ server/src/middleware/role.middleware.js
@@ -0,0 +1,21 @@
+
+
+export const roleAuth = (...roles)=>{
+    return (req, res, next)=>{
+        if(!req.user){
+            return res.status(401).json({
+                success:false,
+                message:"Unauthorized"
+            });
+        }
+
+        if(!roles.includes(req.user.role)){
+            return res.status(403).json({
+                success:false,
+                message:"Forbidden: Insufficient permissions"
+            });
+        }
+
+        next()
+    }
+}
