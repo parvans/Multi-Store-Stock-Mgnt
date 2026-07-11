621e79fb6df107572e7f541ea0898a5a99a28c2a
parvans <parvansajeevan@gmail.com>
2026-07-11 12:12:55 +0530

    added the mongoDB connection URI and connected

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import app from './app.js';
+import connectDB from './config/db.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 
 const  startServer = async()=>{
     try {
+        await connectDB();
         app.listen(PORT,()=>{
             console.log(`Server is running on http://localhost:${PORT}`);
         });
