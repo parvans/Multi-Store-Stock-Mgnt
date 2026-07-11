0908421b7882ee9efbce4d03b759d938f29ca422
parvans <parvansajeevan@gmail.com>
2026-07-11 17:19:37 +0530

    changed

--- server/src/middleware/auth.middleware.js
+++ server/src/middleware/auth.middleware.js
@@ -2,14 +2,25 @@ import jwt from 'jsonwebtoken'
 import { ENV } from '../config/env.js'
 
 const auth = async(req,res,next)=>{
-    const token=req.header("Authorization")
-    if(!token) return res.status(403).json({message:'forbidden - token is unavailable'}) 
+    const authHeader = req.header("Authorization");
+
+    if(!authHeader || !authHeader.startsWith("Bearer")){
+        return res.status(401).json({
+            success:false,
+            message:"Authorization token missing"
+        });
+    }
+    const token = authHeader.split(" ")[1];
+
     try {
         const decoded=jwt.verify(token,ENV.JWT_SECRET)
         req.user=decoded
         next(); 
     } catch (error) { 
-        res.status(401).json({message:error.message})
+        res.status(401).json({
+            success:false,
+            message:error.message || "Invalid or expired token"
+        })
     } 
 }
 
