febcf548b58cdb4bbb2c039a6e96510329352f26
parvans <parvansajeevan@gmail.com>
2026-07-12 20:36:35 +0530

    added protect route

---
+++ client/src/components/layout/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+import useAuth from '@/hooks/useAuth'
+import { Navigate } from 'react-router-dom';
+
+export default function ProtectedRoute({children}) {
+    const {user} = useAuth();
+    if(!user){
+        return <Navigate to={"/login"} replace/>
+    }
+
+    return children
+}
