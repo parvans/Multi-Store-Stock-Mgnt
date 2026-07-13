157da1afd0beebd298eb774e89b8c6dbec17b18c
parvans <parvansajeevan@gmail.com>
2026-07-13 12:59:25 +0530

    created a AppRoutes component seperatly for routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,14 @@
-import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
-import Products from './pages/Products'
-import Stores from './pages/Stores'
-import Stocks from './pages/Stocks'
-import ProtectedRoute from './components/layout/ProtectedRoute'
-import DashboardLayout from './layouts/DashboardLayout'
+import { Toaster } from './components/ui/sonner'
+import AppRoutes from './routes/AppRoutes'
 
 function App() {
 
   return (
-    <Routes>
-      <Route path="/login" element={<Login/>} />
-      <Route
-      element={
-        <ProtectedRoute>
-          <DashboardLayout/>
-        </ProtectedRoute>
-      }
-      >
-        <Route index element={<Dashboard/>} />
-        <Route path='products' element={<Products/>} />
-        <Route path='stores' element={<Stores/>} />
-        <Route path='stocks' element={<Stocks/>} />
-      </Route>
-    </Routes>
+    <>
+      <Toaster richColors postion={"top-right"} />
+      <AppRoutes/>
+    </>
   )
 }
 
---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,31 @@
+import ProtectedRoute from '@/components/layout/ProtectedRoute'
+import DashboardLayout from '@/layouts/DashboardLayout'
+import Dashboard from '@/pages/Dashboard'
+import Login from '@/pages/Login'
+import Products from '@/pages/Products'
+import Stocks from '@/pages/Stocks'
+import Stores from '@/pages/Stores'
+import { Route, Routes } from 'react-router-dom'
+
+function AppRoutes() {
+
+  return (
+    <Routes>
+      <Route path="/login" element={<Login/>} />
+      <Route
+      element={
+        <ProtectedRoute>
+          <DashboardLayout/>
+        </ProtectedRoute>
+      }
+      >
+        <Route index element={<Dashboard/>} />
+        <Route path='products' element={<Products/>} />
+        <Route path='stores' element={<Stores/>} />
+        <Route path='stocks' element={<Stocks/>} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default AppRoutes;
