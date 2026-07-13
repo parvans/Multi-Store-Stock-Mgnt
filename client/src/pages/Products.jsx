58958a8ea7262cd8d19bd5dbd49e806c15793ccc
parvans <parvansajeevan@gmail.com>
2026-07-13 23:47:27 +0530

    started Product page

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -1,8 +1,16 @@
+import { Package } from "lucide-react";
 
 export default function Products() {
   return (
     <div>
-        Products
+        <div className="flex items-center justify-between">
+          <h1 className="flex items-center gap-3 text-2xl font-bold">
+            <Package className="w-6 h-6"/>
+            Products
+          </h1>
+        </div>
+
+        
     </div>
   )
 }
